9eb54bd2beca6c1216a40a9bb265395c433e2866
ThomasDumazet <thomas.dumazet@gmail.com>
2021-07-09 16:47:06 +0200

    Hotfix - student profile image not loaded when adding students during classroom creation

--- classroom/assets/js/scripts/manageClassroom.js
+++ classroom/assets/js/scripts/manageClassroom.js
@@ -767,7 +767,7 @@ function actualizeStudentActivities(activity, correction) {
 function addStudentRow(pseudo) {
     return `
     <li class="row align-items-center my-1 ">
-        <img class="col-2 propic" src="/public/content/img/alphabet/` + pseudo.slice(0, 1).toUpperCase() + `.png" alt="Photo de profil">
+        <img class="col-2 propic" src="/classroom/assets/media/alphabet/` + pseudo.slice(0, 1).toUpperCase() + `.png" alt="Photo de profil">
         <div class="col">` + pseudo + `</div>
         <button type=\"button\" class=\"btn btn-danger remove-student h-50\" data-toggle=\"tooltip\" data-placement=\"top\"  >
             <i class=\"fas fa-times\"></i>
