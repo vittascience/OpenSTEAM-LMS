7cc9a85bf87a34957b2de895cb21e61cffad4f42
ThomasDumazet <thomas.dumazet@gmail.com>
2021-08-27 11:50:28 +0200

    Bugfix when there are missing folder.s in plugin.s

--- gulp/classroomTasks.js
+++ gulp/classroomTasks.js
@@ -382,13 +382,17 @@ class AutoBuildManager {
     async readFolderForList(plugin, folder, list) {
         return new Promise((resolve, reject) => {
             fs.readdir(folder, (err, files) => {
-                try {
-                    files.forEach(file => {
-                        this.pluginsList[this.pluginsList.indexOf(plugin)][list].push(file);
-                    });
-                    resolve();
-                } catch (error) {
-                    reject(error);
+                if(files){
+                    try {
+                        files.forEach(file => {
+                            this.pluginsList[this.pluginsList.indexOf(plugin)][list].push(file);
+                        });
+                        resolve();
+                    } catch (error) {
+                        reject(error);
+                    }
+                }else{
+                    console.error(`Folder ${folder} doesn't exist! Skipping!`);
                 }
             });
         }).catch((error) => {
