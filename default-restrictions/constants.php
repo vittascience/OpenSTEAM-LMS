fdc104c8932052400d5f6e690c1846212a4f671e
Arouz <cointe.remi@gmail.com>
2021-10-07 19:14:54 +0200

    switch constant file restriction to a table in database

--- default-restrictions/constants.php
+++
@@ -1,4 +0,0 @@
-<?php
-define("userDefaultRestrictions", ['maxStudents' => 25]);
-define("groupDefaultRestrictions", ['maxTeachers' => 10, 'maxStudents' => 500, 'maxStudentsPerTeacher' => 50]);
-define("activitiesRestrictions", []); // exampke : define("activitiesRestrictions", ["vittascience" => 5, "vittascience2" => 10]);
