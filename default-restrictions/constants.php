e691a42302dca650b6147319f214b35b9aa5128c
Arouz <cointe.remi@gmail.com>
2021-10-07 15:28:06 +0200

    add default for activities

--- default-restrictions/constants.php
+++ default-restrictions/constants.php
@@ -1,3 +1,4 @@
 <?php
 define("userDefaultRestrictions", ['maxStudents' => 25]);
 define("groupDefaultRestrictions", ['maxTeachers' => 10, 'maxStudents' => 500, 'maxStudentsPerTeacher' => 50]);
+define("activitiesRestrictions", []); // exampke : define("activitiesRestrictions", ["vittascience" => 5, "vittascience2" => 10]);
