05b876c6ad775d7cf4a29ea70205e521cd3558f5
Maxence Lavenu <maxence.lavenu@outlook.fr>
2022-04-22 16:06:35 +0200

    Updated language switcher to add Language names.

--- classroom/assets/js/constants/modals.js
+++ classroom/assets/js/constants/modals.js
@@ -96,9 +96,15 @@ const classroomModals = {
             title: 'classroom.modals.settingsTeacher.title'
         },
         content: `<h4 data-i18n="classroom.modals.settingsTeacher.lang">Langue</h4>
-        <div id="switch-lang-list" class="d-flex justify-content-center">
-            <img class="flags-item mr-2" alt="flag" src="assets/media/flags/fr.png" onclick="changeLang('fr')">
-            <img class="flags-item" alt="flag" src="assets/media/flags/en.png" onclick="changeLang('en')">
+        <div id="switch-lang-list">
+            <div class="flags-item">
+                <img alt="flag" src="assets/media/flags/fr.png" onclick="changeLang('fr')">
+                <span>Français</span>
+            </div>
+            <div class="flags-item">
+                <img alt="flag" src="assets/media/flags/en.png" onclick="changeLang('en')">
+                <span>English</span>
+            </div>
         </div>
         <h4 data-i18n="classroom.modals.settingsTeacher.password">Mot de passe</h4>
         <p class="text-center" data-i18n="classroom.modals.settingsTeacher.passwordDescription"></p>
@@ -127,9 +133,15 @@ const classroomModals = {
                     */
         content: `<div>
                     <h4 data-i18n="classroom.modals.settingsTeacher.lang">Langue</h4>
-                    <div id="switch-lang-list" class="d-flex justify-content-center">
-                    <img class="flags-item  mr-2" alt="flag" src="assets/media/flags/fr.png" onclick="changeLang('fr')">
-                    <img class="flags-item" alt="flag" src="assets/media/flags/en.png" onclick="changeLang('en')">
+                    <div id="switch-lang-list">
+                        <div class="flags-item">
+                            <img alt="flag" src="assets/media/flags/fr.png" onclick="changeLang('fr')">
+                            <span>Français</span>
+                        </div>
+                        <div class="flags-item">
+                            <img alt="flag" src="assets/media/flags/en.png" onclick="changeLang('en')">
+                            <span>English</span>
+                        </div>
                     </div>
                     <h4 data-i18n="classroom.modals.settingsTeacher.description">Pour modifier votre mot de passe ou d'autres paramètres de votre compte</h4>
                     <div class="d-flex flex-column align-items-center" style="gap: 0.5rem;">
