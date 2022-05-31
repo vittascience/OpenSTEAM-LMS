4649b013798377b6d02dad04306dd752ad8b17ce
Arouz <48770524+Arouz@users.noreply.github.com>
2022-05-31 19:02:38 +0200

    removed wrong teacherFolder function

--- classroom/assets/js/scripts/dashboardActivities.js
+++ classroom/assets/js/scripts/dashboardActivities.js
@@ -197,30 +197,6 @@ function teacherFolder(folder, displayStyle) {
     return content;
 }
 
-function teacherFolder(folder) {
-    let html = `<div class="activity-item activity-teacher">
-        <div class="activity-card folder">
-            <div class="activity-card-top">
-                <div class="dropdown"><i class="fas fa-cog fa-2x" type="button" id="dropdown-activityItem-${folder.id}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
-                <div class="dropdown-menu" aria-labelledby="dropdown-activityItem-${folder.id}" data-id="${folder.id}">
-                    <li class="classroom-clickable col-12 dropdown-item" href="#" onclick="folders.updateFolder(${folder.id})" style="border-bottom:2px solid rgba(0,0,0,.15">Modifier</li>
-                    <li class="classroom-clickable col-12 dropdown-item" href="#" onclick="folders.deleteFolder(${folder.id})">Supprimer</li>
-                </div>
-            </div>
-        </div>
-            <div class="activity-card-mid">
-            </div>
-            <div class="activity-card-bot">
-                <div class="info-tutorials" data-id="${folder.id}">
-                </div>
-            </div>
-        </div>
-        <h3 data-toggle="tooltip" title="${folder.name}" class="activity-item-title">${folder.name}</h3>
-    </div>`
-
-    return html;
-}
-
 function classeItem(classe, nbStudents, students) {
     function maxLength(array) {
         let count = 0
