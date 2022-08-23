ad463b5bd9c7d98871683018c156628bcfb31f20
Arouz <48770524+Arouz@users.noreply.github.com>
2022-08-23 13:09:30 +0200

    parcours

--- routing/Routing.php
+++ routing/Routing.php
@@ -25,21 +25,22 @@ use Learn\Controller\ControllerFavorite;
 use Learn\Controller\ControllerCollection;
 use Interfaces\Controller\ControllerProject;
 use Classroom\Controller\ControllerClassroom;
-use Utils\Exceptions\EntityOperatorException;
-
-
-
-use Classroom\Controller\ControllerGroupAdmin;
-use Classroom\Controller\ControllerSuperAdmin;
-
 use Learn\Controller\ControllerNewActivities;
 
+
+
+use Utils\Exceptions\EntityOperatorException;
+use Classroom\Controller\ControllerGroupAdmin;
+
+use Classroom\Controller\ControllerSuperAdmin;
+
 use Learn\Controller\ControllerCourseLinkCourse;
+use Classroom\Controller\ControllerCourseLinkUser;
 use Utils\Exceptions\EntityDataIntegrityException;
 use Classroom\Controller\ControllerActivityLinkUser;
 use Interfaces\Controller\ControllerProjectLinkUser;
 use Classroom\Controller\ControllerClassroomLinkUser;
-use Utils\Controller\ControllerUpload;
+
 
 $dotenv = Dotenv::createImmutable(__DIR__ . "/../");
 $dotenv->safeLoad();
@@ -57,12 +58,9 @@ try {
     session_start();
     $user = null;
     if (isset($_SESSION["id"])) {
-        $user = $entityManager->getRepository('User\Entity\User')->find(intval($_SESSION["id"]))->jsonSerialize();
-        $storedClassroom = $entityManager->getRepository('Classroom\Entity\ClassroomLinkUser')->findOneBy(['user' => $_SESSION["id"]]);
-        if ($storedClassroom) {
-            $classroom = $storedClassroom->jsonSerialize();
-            $user["classroom"] = $classroom["classroom"]["id"];
-        }
+        $user = $entityManager->getRepository('User\Entity\User')
+            ->find(intval($_SESSION["id"]))->jsonSerialize();
+
         try {
             $regular = $entityManager->getRepository('User\Entity\Regular')
                 ->find(intval($_SESSION["id"]))->jsonSerialize();
@@ -92,18 +90,18 @@ try {
     // get and scan the entire plugins folder
     $pluginsDir = '../plugins';
     if (is_dir($pluginsDir)) {
-        $pluginsFound = array_diff(scandir($pluginsDir), array('..', '.', '.DS_Store'));
+        $pluginsFound = array_diff(scandir($pluginsDir), array('..', '.'));
 
         // scan each single plugin folder
         foreach ($pluginsFound as $singlePlugin) {
-            $singlePluginFolders = array_diff(scandir("../plugins/$singlePlugin"), array('..', '.', '.DS_Store'));
+            $singlePluginFolders = array_diff(scandir("../plugins/$singlePlugin"), array('..', '.'));
 
             // convert snake_case from url param into PascalCase to find the right controller file to instanciate
             $ControllerToInstanciate = "Controller" . str_replace('_', '', ucwords($controller, '_'));
 
             // check if a Controller folder exists in the plugins list
             if (in_array("Controller", $singlePluginFolders)) {
-                // check if the required controller file exists and require it
+                // check if the required controller file exists and require it 
                 if (file_exists("../plugins/$singlePlugin/Controller/$ControllerToInstanciate.php")) {
                     require_once "../plugins/$singlePlugin/Controller/" . $ControllerToInstanciate . ".php";
 
@@ -211,12 +209,10 @@ try {
             echo (json_encode($controller->action($action, $_POST)));
             $log->info($action, OK);
             break;
-        case 'upload': 
-            $action = lcfirst(str_replace('_', '', ucwords($action, '_')));
-            $controllerUpload = new ControllerUpload($entityManager, $user);
-            echo json_encode(call_user_func(
-                array($controllerUpload,$action)
-            ));
+        case 'user_link_course':
+            $controller = new ControllerCourseLinkUser($entityManager, $user);
+            echo (json_encode($controller->action($action, $_POST)));
+            $log->info($action, OK);
             break;
         default:
             $log->warning(null, __FILE__, __LINE__, "Non matched controller");
