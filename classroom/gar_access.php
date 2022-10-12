eee40e66d643400a07e7d46a50170a12520df0a7
ultimecreation <naserbechata@gmail.com>
2022-10-12 10:47:24 +0200

    removed cdn calls for font awesome 5.1.1

--- classroom/gar_access.php
+++ classroom/gar_access.php
@@ -14,7 +14,7 @@ $user = ConnectionManager::getSharedInstance()->checkConnected(); ?>
     <link rel="stylesheet" href="/assets/js/lib/bootstrap-4.4.1/bootstrap.min.css">
     <link rel='stylesheet' type='text/css' href='/classroom/assets/css/main.css' />
     <script src="assets/js/lib/marked.min.js"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/lib/fontawesome-5.1.1/all.css">
     <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 </head>
 
