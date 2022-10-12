29be392044d55c44d822c35186ccb6c154fb92ae
ultimecreation <naserbechata@gmail.com>
2022-10-12 10:42:41 +0200

    removed cdn calls for jquery 3.5.1

--- classroom/privacy.php
+++ classroom/privacy.php
@@ -17,8 +17,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css"
         integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
     <!-- Material icon -->
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
-        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="/assets/js/lib/jquery-3.5.1/jquery-3.5.1.min.js"></script>
     <script src="/assets/js/lib/popper-1.16.0/popper.min.js">
     </script>
     <link rel="stylesheet" href="assets/css/theme.css">
