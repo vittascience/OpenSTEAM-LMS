39187012a430fd40125de6b7dbd4b5d43fc0e51c
ultimecreation <naserbechata@gmail.com>
2022-10-12 10:47:24 +0200

    removed cdn calls for font awesome 5.1.1

--- classroom/legal.php
+++ classroom/legal.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
     <link rel="stylesheet" href="/assets/js/lib/bootstrap-4.1.1/bootstrap.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/lib/fontawesome-5.1.1/all.css">
     <!-- Material icon -->
     <script src="/assets/js/lib/jquery-3.5.1/jquery-3.5.1.min.js"></script>
     <script src="/assets/js/lib/popper-1.16.0/popper.min.js">
