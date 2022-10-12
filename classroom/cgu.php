b6d9efd4f7435792fd71ca9d6d039a3bc946c97c
ultimecreation <naserbechata@gmail.com>
2022-10-12 10:05:49 +0200

    removed cdn calls for popper 1.16.0

--- classroom/cgu.php
+++ classroom/cgu.php
@@ -19,8 +19,7 @@
     <!-- Material icon -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    <script src="/assets/js/lib/popper-1.16.0/popper.min.js">
     </script>
     <link rel="stylesheet" href="assets/css/theme.css">
     <link rel="stylesheet" href="assets/css/login.css">
