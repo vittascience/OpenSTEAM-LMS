5ff905d0326fe938f6e26d092b9b2bbc7932f02f
Andreas101010 <feronandreas@gmail.com>
2022-10-24 12:34:24 +0200

    Update cookies translate ES

--- classroom/assets/js/utils/translate.js
+++ classroom/assets/js/utils/translate.js
@@ -2,7 +2,7 @@ i18next.use(window.i18nextXHRBackend)
     .init({
         debug: false,
         lng: getCookie('lng'),
-        whitelist: ['fr', 'en'],
+        whitelist: ['fr', 'en','es'],
         fallbackLng: 'fr',
         backend: {
             loadPath: _PATH + 'assets/lang/{{lng}}/ns.json'
