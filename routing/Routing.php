e862ef066b99e5e5b22d045f09a36b0558ef648b
StrippoliJules <julesstrippoli@gmail.com>
2024-07-25 12:09:35 +0200

    modify switch

--- routing/Routing.php
+++ routing/Routing.php
@@ -134,16 +134,16 @@ try {
         case 'project_link_user':
             $controller = new ControllerProjectLinkUser($entityManager, $user);
             echo (json_encode($controller->action($action, $_POST)));
-            error_log("User data: " . print_r($user, true)); 
-            $session_id = session_id();
-            $sessionRepository = $entityManager->getRepository(Session::class);
-            $sessionRepository->createSession($session_id, $user['id']);
-            echo (json_encode(["session_id" => $session_id]));
             $log->info($action, OK);
             break;
         case 'classroom':
             $controller = new ControllerClassroom($entityManager, $user);
             echo (json_encode($controller->action($action, $_POST)));
+            error_log("User data: " . print_r($user, true)); 
+            $session_id = session_id();
+            $sessionRepository = $entityManager->getRepository(Session::class);
+            $sessionRepository->createSession($session_id, $user['id']);
+            echo (json_encode(["session_id" => $session_id]));
             $log->info($action, OK);
             break;
         case 'classroom_link_user':
