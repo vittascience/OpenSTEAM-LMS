63948430733ac6a78d99f8bb91eb7f063b4423a3
Paul Libbrecht <paul@hoplahup.net>
2025-04-14 11:28:31 +0200

    save draft when switching page now implemented.

--- classroom/assets/js/scripts/displayPanel.js
+++ classroom/assets/js/scripts/displayPanel.js
@@ -438,7 +438,7 @@ function getTeacherActivity() {
             
             // LTI Activity
             if (Activity.isLti) {
-                launchLtiResource(Activity.id, Activity.type, JSON.parse(Activity.content).description, Activity.isAutocorrect, Activity.url, Activity.content);
+                launchLtiResource(Activity.id, Activity.type, JSON.parse(Activity.content).description, Activity.isAutocorrect, Activity.url);
             } else {
                 // Non core and non LTI Activity fallback
                 $("#activity-content").html(bbcodeToHtml(contentParsed));
